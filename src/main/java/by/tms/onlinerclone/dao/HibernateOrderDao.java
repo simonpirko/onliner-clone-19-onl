3d704448a3be26fce0487b2957da0eae76f1f1e2
VDN_dude <garanovich2@gmail.com>
2023-06-29 12:29:19 +0300

    added OrderService, HibernateOrderDao

---
+++ src/main/java/by/tms/onlinerclone/dao/HibernateOrderDao.java
@@ -0,0 +1,48 @@
+package by.tms.onlinerclone.dao;
+
+import by.tms.onlinerclone.entity.Order;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@Transactional
+public class HibernateOrderDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void save(Order order){
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.save(order);
+    }
+
+    public void update(Order order){
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.update(order);
+    }
+
+    @Transactional(readOnly = true)
+    public Order findById(long id){
+        Session currentSession = sessionFactory.getCurrentSession();
+        return currentSession.get(Order.class, id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Order> findByUserId(long id){
+        Session currentSession = sessionFactory.getCurrentSession();
+        Query<Order> query = currentSession.createQuery("from Order where user_id = :id", Order.class);
+        query.setParameter("id", id);
+        return query.getResultList();
+    }
+
+    public void delete(Order order){
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.delete(order);
+    }
+}

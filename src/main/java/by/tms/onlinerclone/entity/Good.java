333665e21d4e59c8441d3c764a9a0d7e07b8b2a2
Yuri-Vanderon <garanovich2@gmail.com>
2023-07-04 14:07:34 +0300

    added price & photos to Good

--- src/main/java/by/tms/onlinerclone/entity/Good.java
+++ src/main/java/by/tms/onlinerclone/entity/Good.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -19,9 +20,14 @@ public class Good {
 
     private String name;
 
+    private Double price;
+
     @ManyToOne(cascade = CascadeType.ALL)
     private GoodCategory category;
 
     @OneToMany
     private List<GoodCharacters> characters;
+
+    @ElementCollection
+    private List<byte[]> photos = new ArrayList<>();
 }

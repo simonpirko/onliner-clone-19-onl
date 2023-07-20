9e8144057b1485562df1d5856a7bd34520d5066c
Yuri-Vanderon <garanovich2@gmail.com>
2023-07-20 16:30:12 +0300

    changed annotations

--- src/main/java/by/tms/onlinerclone/entity/PageableGoods.java
+++ src/main/java/by/tms/onlinerclone/entity/PageableGoods.java
@@ -1,10 +1,13 @@
 package by.tms.onlinerclone.entity;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 public class PageableGoods {
 
     public List<Good> goodList;

2088586333a9198c223244709aaffdb7448cecf3
Yuri-Vanderon <garanovich2@gmail.com>
2023-07-20 16:21:04 +0300

    changed annotations & List<GoodCharacters> to Set<...> & added patterns

--- src/main/java/by/tms/onlinerclone/dto/GoodCreatorDto.java
+++ src/main/java/by/tms/onlinerclone/dto/GoodCreatorDto.java
@@ -1,24 +1,31 @@
 package by.tms.onlinerclone.dto;
 
-import by.tms.onlinerclone.entity.GoodCategory;
 import by.tms.onlinerclone.entity.GoodCharacters;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Set;
 
-@Data
+@Getter
+@Setter
 public class GoodCreatorDto {
 
+    @Pattern(regexp = "[A-Za-z&$#@*., -!?+=()\\[\\]{}<>\\d]*", message = "This good name contains invalid characters")
     private String name;
 
+    @Pattern(regexp = "[A-Za-z&$#@*., -!?+=()\\[\\]{}<>\\d]*", message = "This description contains invalid characters")
     private String description;
 
     private BigDecimal price;
 
-    private GoodCategory category;
+    @Pattern(regexp = "([A-Za-z])*", message = "This category contains invalid characters")
+    private String category;
 
-    private List<GoodCharacters> characters;
+    private Set<GoodCharacters> characters;
 
-    private List<byte[]> photos;
+    private List<MultipartFile> photos;
 }

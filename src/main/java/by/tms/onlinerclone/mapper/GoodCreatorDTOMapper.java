0c7babeb362deeb4493c5249da712d129b77fa3d
Yuri-Vanderon <garanovich2@gmail.com>
2023-07-20 16:21:37 +0300

    added GoodCreatorDTOMapper

---
+++ src/main/java/by/tms/onlinerclone/mapper/GoodCreatorDTOMapper.java
@@ -0,0 +1,29 @@
+package by.tms.onlinerclone.mapper;
+
+import by.tms.onlinerclone.dto.GoodCreatorDto;
+import by.tms.onlinerclone.entity.Good;
+import lombok.SneakyThrows;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GoodCreatorDTOMapper {
+
+    @SneakyThrows
+    public static Good goodCreatorDtoToGood(GoodCreatorDto goodCreator){
+
+        List<byte[]> photos = new ArrayList<>();
+
+        for (MultipartFile photo : goodCreator.getPhotos()) {
+            photos.add(photo.getBytes());
+        }
+
+        return Good.builder()
+                .name(goodCreator.getName())
+                .description(goodCreator.getDescription())
+                .price(goodCreator.getPrice())
+                .photos(photos)
+                .build();
+    }
+}

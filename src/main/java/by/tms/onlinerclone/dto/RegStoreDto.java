f5238c23dd8f6a9121f2aacb98b42f80985edaed
Yuri-Vanderon <garanovich2@gmail.com>
2023-07-20 16:31:09 +0300

    added variable logo & changed pattern and annotations

--- src/main/java/by/tms/onlinerclone/dto/RegStoreDto.java
+++ src/main/java/by/tms/onlinerclone/dto/RegStoreDto.java
@@ -1,15 +1,20 @@
 package by.tms.onlinerclone.dto;
 
 import by.tms.onlinerclone.entity.SessionUser;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.Pattern;
 
-@Data
+@Getter
+@Setter
 public class RegStoreDto {
 
-    @Pattern(regexp = "([A-Za-z&$#@*.,\\d ]\\-?)*", message = "This store name contains invalid characters")
+    @Pattern(regexp = "[A-Za-z&$#@*., -!?+=()\\[\\]{}<>\\d]*", message = "This store name contains invalid characters")
     private String name;
+
+    private MultipartFile logo;
 
     private SessionUser user;
 }

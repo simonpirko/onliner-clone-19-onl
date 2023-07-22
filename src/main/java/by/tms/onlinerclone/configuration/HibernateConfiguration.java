acbf42b815e77195a82ae009b4c2576d3a6f5a9d
andrlis <andrey.lisouski@gmail.com>
2023-07-22 22:44:15 +0300

    [c19-20] Restore HibernateConfiguration

---
+++ src/main/java/by/tms/onlinerclone/configuration/HibernateConfiguration.java
@@ -0,0 +1,55 @@
+package by.tms.onlinerclone.configuration;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * @author Simon Pirko on 27.06.23
+ */
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName("org.postgresql.Driver");
+        basicDataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
+        basicDataSource.setUsername("postgres");
+        basicDataSource.setPassword("root");
+        return basicDataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactoryBean(){
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(dataSource());
+        localSessionFactoryBean.setPackagesToScan("by.tms");
+        localSessionFactoryBean.setHibernateProperties(hibernateProperties());
+        return localSessionFactoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(){
+        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+        hibernateTransactionManager.setSessionFactory(sessionFactoryBean().getObject());
+        return hibernateTransactionManager;
+    }
+
+    private Properties hibernateProperties(){
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto" , "update");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty("show_sql", "true");
+        return properties;
+    }
+}
